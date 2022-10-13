aae6886c2e18901bf8315e13350bcd5462aa6d01
daniellourencoedu <daniellourencoedu@gmail.com>
2022-10-13 17:31:37 -0300

    starting new project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+
+import { Layout } from "../components";
+import { LanguageProvider } from "../context";
+
+import "../styles/globals.css";
+import "../styles/variables.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<LanguageProvider>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</LanguageProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
