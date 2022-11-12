ee15dac670015a9c890394b0626ebb473a83e09d
daniellourencoedu <daniellourencoedu@gmail.com>
2022-11-12 01:00:02 -0300

    start adding topping useState

--- components/modals/AddPizzaModal.tsx
+++ components/modals/AddPizzaModal.tsx
@@ -5,31 +5,55 @@ import { FaTimes } from "react-icons/fa";
 import { Pizza } from "../../interface";
 
 import styles from "./addPizzaModal.module.css";
+import { log } from "console";
 
 interface ModalProps {
 	setOpen: (value: boolean) => void;
 }
-
+interface PizzaDefault {
+	title: string;
+	img: string;
+	prices: number[];
+	desc: string;
+	extraOptions: ExtraOptionDefault[];
+}
+interface ExtraOptionDefault {
+	topping: string;
+	price: number[];
+}
 export const AddPizzaModal = ({ setOpen }: ModalProps) => {
-	// const pizzaDefault = {
-	// 	prices: [],
-	// };
+	const pizzaDefault: PizzaDefault = {
+		title: "",
+		img: "",
+		prices: [],
+		desc: "",
+		extraOptions: [{ topping: "", price: [] }],
+	};
 
-	const [newPizza, setNewPizza] = useState({} as Pizza);
+	const [newPizza, setNewPizza] = useState<PizzaDefault>(pizzaDefault);
 
-	const handleNewPizza = (key: string | number, value: string | number) => {
+	const handleNewPizza = (key: string | number, value: any, index: number) => {
 		if (key === "title" || key === "desc" || key === "img") {
 			setNewPizza({ ...newPizza, [key]: value });
 		}
 
-		if (key === 0 || key === 1 || key === 2) {
-			setNewPizza((prev) => {
-				prev.prices.push(value);
-				return { ...prev };
+		if (key === "prices") {
+			const updatedPrices = newPizza.prices;
+			updatedPrices[index] = value;
+			setNewPizza({
+				...newPizza,
+				prices: updatedPrices,
 			});
 		}
+		if (key === "extrasTopping") {
+			const extras = { topping: value, price: [] };
+
+			setNewPizza({ ...newPizza, extraOptions: [extras] });
+		}
 	};
-	console.log(newPizza.prices);
+
+	const handleNewTopping = () => {};
+	console.log(newPizza);
 
 	return (
 		<form className={styles.addModal}>
@@ -42,44 +66,68 @@ export const AddPizzaModal = ({ setOpen }: ModalProps) => {
 				type="file"
 				id="img"
 				name="img"
-				onChange={(e) => handleNewPizza("title", e.target.value)}
+				onChange={(e) => handleNewPizza("img", e.target.value, 0)}
 			/>
 			<label htmlFor="title">title</label>
 			<input
 				type="text"
-				onChange={(e) => handleNewPizza("title", e.target.value)}
+				onChange={(e) => handleNewPizza("title", e.target.value, 0)}
 			/>
 			<label htmlFor="desc">description</label>
 			<textarea
 				name="desc"
 				id="desc"
 				rows={10}
-				onChange={(e) => handleNewPizza("desc", e.target.value)}
+				onChange={(e) => handleNewPizza("desc", e.target.value, 0)}
 			/>
 			<label htmlFor="prices">prices</label>
 			<div className={styles.prices}>
 				<input
 					type="number"
 					placeholder="small"
-					onChange={(e) => handleNewPizza(0, +e.target.value)}
+					value={newPizza.prices[0]}
+					onChange={(e) => handleNewPizza("prices", +e.target.value, 0)}
 				/>
 				<input
 					type="number"
 					placeholder="medium"
-					onChange={(e) => handleNewPizza(1, +e.target.value)}
+					onChange={(e) => handleNewPizza("prices", +e.target.value, 1)}
 				/>
 				<input
 					type="number"
 					placeholder="large"
-					onChange={(e) => handleNewPizza(2, +e.target.value)}
+					onChange={(e) => handleNewPizza("prices", +e.target.value, 2)}
 				/>
 			</div>
 			<label htmlFor="extras">extras</label>
 			<div className={styles.extras}>
 				<div className={styles.extrasInputs}>
-					<input type="text" name="extra" />
-					<input type="number" name="extraPrice" placeholder="value" />
-					<button>add</button>
+					<input
+						type="text"
+						name="extras"
+						onChange={(e) => handleNewPizza("extrasTopping", e.target.value, 0)}
+					/>
+					<input
+						type="number"
+						name="extraPrice"
+						placeholder="small"
+						onChange={(e) => handleNewTopping("extras", +e.target.value, 0)}
+					/>
+					<input
+						type="number"
+						name="extraPrice"
+						placeholder="medium"
+						onChange={(e) => handleNewTopping("extras", +e.target.value, 1)}
+					/>
+					<input
+						type="number"
+						name="extraPrice"
+						placeholder="large"
+						onChange={(e) => handleNewTopping("extras", +e.target.value, 2)}
+					/>
+					<button onClick={() => console.log(newPizza.extraOptions)}>
+						add
+					</button>
 				</div>
 				<div className={styles.extrasIng}></div>
 			</div>
