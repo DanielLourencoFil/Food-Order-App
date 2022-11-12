1c22b085583053ec6ae7c65b171ecd4a4bb99a38
daniellourencofil <daniellourencoedu@gmail.com>
2022-11-11 23:21:16 -0300

    add modal useState

--- components/modals/AddPizzaModal.tsx
+++ components/modals/AddPizzaModal.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import { useState } from "react";
 
 import { FaTimes } from "react-icons/fa";
+
+import { Pizza } from "../../interface";
 
 import styles from "./addPizzaModal.module.css";
 
@@ -9,33 +11,99 @@ interface ModalProps {
 }
 
 export const AddPizzaModal = ({ setOpen }: ModalProps) => {
-	const handleNewPizza = (e: any) => {
-		e.preventDefault();
+	// const pizzaDefault = {
+	// 	prices: [],
+	// };
+
+	const [newPizza, setNewPizza] = useState({} as Pizza);
+
+	const handleNewPizza = (key: string | number, value: string | number) => {
+		if (key === "title" || key === "desc" || key === "img") {
+			setNewPizza({ ...newPizza, [key]: value });
+		}
+
+		if (key === 0 || key === 1 || key === 2) {
+			setNewPizza((prev) => {
+				prev.prices.push(value);
+				return { ...prev };
+			});
+		}
 	};
+	console.log(newPizza.prices);
+
 	return (
-		<form className={styles.addModal} onClick={(e) => handleNewPizza(e)}>
+		<form className={styles.addModal}>
 			<button className={styles.closeBtn} onClick={() => setOpen(false)}>
 				<FaTimes className={styles.closeBtnIcon} />
 			</button>
 			<h1 className={styles.title}>add new pizza</h1>
-			<label htmlFor="image">Image</label>
-			<input type="file" id="image" name="image" />
+			<label htmlFor="img">Image</label>
+			<input
+				type="file"
+				id="img"
+				name="img"
+				onChange={(e) => handleNewPizza("title", e.target.value)}
+			/>
 			<label htmlFor="title">title</label>
-			<input type="text" />
+			<input
+				type="text"
+				onChange={(e) => handleNewPizza("title", e.target.value)}
+			/>
 			<label htmlFor="desc">description</label>
-			<textarea name="desc" id="desc" rows={10} />
+			<textarea
+				name="desc"
+				id="desc"
+				rows={10}
+				onChange={(e) => handleNewPizza("desc", e.target.value)}
+			/>
 			<label htmlFor="prices">prices</label>
 			<div className={styles.prices}>
-				<input type="number" placeholder="small" />
-				<input type="number" placeholder="medium" />
-				<input type="number" placeholder="large" />
+				<input
+					type="number"
+					placeholder="small"
+					onChange={(e) => handleNewPizza(0, +e.target.value)}
+				/>
+				<input
+					type="number"
+					placeholder="medium"
+					onChange={(e) => handleNewPizza(1, +e.target.value)}
+				/>
+				<input
+					type="number"
+					placeholder="large"
+					onChange={(e) => handleNewPizza(2, +e.target.value)}
+				/>
 			</div>
 			<label htmlFor="extras">extras</label>
 			<div className={styles.extras}>
-				<input type="text" name="extra" />
-				<input type="number" name="extraPrice" placeholder="value" />
-				<button>add</button>
+				<div className={styles.extrasInputs}>
+					<input type="text" name="extra" />
+					<input type="number" name="extraPrice" placeholder="value" />
+					<button>add</button>
+				</div>
+				<div className={styles.extrasIng}></div>
 			</div>
 		</form>
 	);
 };
+
+/*
+export interface Pizza {
+	_id: string;
+	title: string;
+	img: string;
+	prices: number[];
+	desc: string;
+	extraOptions: ExtraOption[];
+	createdAt: Date | string;
+	updatedAt: Date | string;
+	__v: number;
+}
+
+export interface ExtraOption {
+	topping: string;
+	price: number[];
+	_id: string;
+}
+
+*/
